088f43030e45290ff293bd0abfb196cc67dcbb4a
Carlo <qaszz1998@gmail.com>
2020-09-01 20:06:09 +0200

    made ONES pass, TWOS fail

--- src/main/java/no/kristiania/yatzy/YatzyGame.java
+++ src/main/java/no/kristiania/yatzy/YatzyGame.java
@@ -2,7 +2,7 @@ package no.kristiania.yatzy;
 
 public class YatzyGame {
     public int score(YatzyCategory ones, int[] dice) {
-        int result = 4;
+        int result = 0;
         for (int die : dice){
             if (die == 1){
                 result +=1;
