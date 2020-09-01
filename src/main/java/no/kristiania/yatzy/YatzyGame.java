1774db105e645320e9d13f78c5b1771c15243275
Carlo <qaszz1998@gmail.com>
2020-09-01 20:21:10 +0200

    TWOS GREEN, fix maven build

--- src/main/java/no/kristiania/yatzy/YatzyGame.java
+++ src/main/java/no/kristiania/yatzy/YatzyGame.java
@@ -1,11 +1,13 @@
 package no.kristiania.yatzy;
 
 public class YatzyGame {
-    public int score(YatzyCategory ones, int[] dice) {
+    public int score(YatzyCategory category, int[] dice) {
         int result = 0;
         for (int die : dice){
-            if (die == 1){
-                result +=1;
+            if (die == 1 && category == YatzyCategory.ONES){
+                result += die;
+            } else if (die == 2 && category == YatzyCategory.TWOS){
+                result += die;
             }
         }
         return result;
